7539569d47952a05c3156ab0b372b192feb5696c
Afik Eschel <afikeschel@gmail.com>
2025-04-29 19:18:18 +0200

    Add Javadoc comments to CalculatorImpl and ICalculator

--- src/main/java/at/fhj/msd/CalculatorImpl.java
+++ src/main/java/at/fhj/msd/CalculatorImpl.java
@@ -1,63 +1,33 @@
 package at.fhj.msd;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-
+/**
+ * Implementation of the ICalculator interface.
+ */
 public class CalculatorImpl implements ICalculator {
 
-    private static final Logger logger = LogManager.getLogger(CalculatorImpl.class);
-
     /**
-     * Easy Calculator for first steps in test automation for Educational Usage
+     * Default constructor.
      */
-    public CalculatorImpl() {
+    public CalculatorImpl() {}
 
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see at.fhj.itm.demo.Calculator#add(java.lang.double, java.lang.double)
-     */
     @Override
     public double add(double number1, double number2) {
-        double result = number1 + number2;
-        logger.info("Adding {} + {} = {}", number1, number2, result);
-        return result;
+        return number1 + number2;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see at.fhj.itm.demo.Calculator#minus(java.lang.double, java.lang.double)
-     */
     @Override
     public double minus(double number1, double number2) {
         return number1 - number2;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see at.fhj.itm.demo.Calculator#divide(java.lang.double, java.lang.double)
-     */
     @Override
     public double divide(double number1, double number2) {
         double quotient = number1 / number2;
-        if (Double.isNaN(quotient) || Double.isInfinite(quotient)) {
-            logger.error("Division by zero: {} / {}", number1, number2);
+        if (Double.isNaN(quotient) || Double.isInfinite(quotient))
             throw new ArithmeticException("Division " + number1 + " / " + number2 + " isn't possible");
-        }
-        logger.info("Dividing {} / {} = {}", number1, number2, quotient);
         return quotient;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see at.fhj.itm.demo.Calculator#multiply(java.lang.double, java.lang.double)
-     */
     @Override
     public double multiply(double number1, double number2) {
         return number1 * number2;
@@ -67,12 +37,10 @@ public class CalculatorImpl implements ICalculator {
     public double factorial(int n) {
         if (n < 0) return 0;
         if (n == 0) return 1;
-
         double result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
         }
         return result;
     }
-
 }
--- src/main/java/at/fhj/msd/ICalculator.java
+++ src/main/java/at/fhj/msd/ICalculator.java
@@ -1,14 +1,47 @@
 package at.fhj.msd;
 
+/**
+ * Interface for a simple calculator providing basic operations.
+ */
 public interface ICalculator {
-  public abstract double add(double number1, double number2);
 
-  public abstract double minus(double number1, double number2);
+    /**
+     * Adds two numbers together.
+     * @param number1 first number
+     * @param number2 second number
+     * @return the sum
+     */
+    double add(double number1, double number2);
 
-  public abstract double divide(double number1, double number2);
+    /**
+     * Subtracts the second number from the first number.
+     * @param number1 first number
+     * @param number2 second number
+     * @return the result
+     */
+    double minus(double number1, double number2);
 
-  public abstract double multiply(double number1, double number2);
+    /**
+     * Divides the first number by the second number.
+     * @param number1 numerator
+     * @param number2 denominator
+     * @return the quotient
+     * @throws ArithmeticException if division by zero
+     */
+    double divide(double number1, double number2);
 
-  double factorial(int n);
+    /**
+     * Multiplies two numbers.
+     * @param number1 first number
+     * @param number2 second number
+     * @return the product
+     */
+    double multiply(double number1, double number2);
 
+    /**
+     * Calculates the factorial of a number.
+     * @param n the number
+     * @return factorial value
+     */
+    double factorial(int n);
 }
